084bd007fe1251d28dfb1a35d8057c66913065d2
Pansuriya Harsh <harsh@Harshs-MacBook-Pro.local>
2023-02-19 02:16:57 +0530

    Added first.cpp

---
+++ first.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+// use fixed << setprecision() for normal formate
+// a*b=gcd(a,b)*lcm(a,b)
+#define ll long long
+#define vc vector
+#define trav(a,x) for(auto& a : x)
+#define all(v) v.begin(), v.end()
+#define f(p,q,r) for(ll p=q;p<r;p++)
+#define fr(p,q,r) for(ll p=q;p>=r;p--)
+#define sz(x) (ll)(x).size()
+#define el endl
+const ll M = 1e18+7;
+#define pb push_back
+#define pp pop_back
+#define max_3(a,b,c) max(a,max(b,c))
+#define min_3(a,b,c) min(a,min(b,c))
+#define max_4(a,b,c,d) max(a,max(b,max(c,d)))
+#define min_4(a,b,c,d) min(a,min(b,min(c,d)))
+#define better_one ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+void swap(ll &a,ll &b){ a=a^b;b=b^a;a=a^b;}
+using namespace std;
+
+void solve()
+{
+    cout<<"hi"<<el;
+}
+
+int main ()
+{
+    // cout << el;
+    better_one;
+    int t=1;
+    // cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Competitive Programming
 
   In this repo I will save my CP codes and codeforces releted things.
+
+## subheader 
+
+  this subheader is made.
